fea7d28d04e47a54c3a140c02eaca62f1349b70c
Anh Hồng <hong4rc@gmail.com>
2021-03-06 09:31:30 +0700

    Up to date

--- gen-host.js
+++ gen-host.js
@@ -9,26 +9,25 @@ const hosts = [
   'https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt',
 
   // Suspicious
-  'https://raw.githubusercontent.com/PolishFiltersTeam/KADhosts/master/KADhosts_without_controversies.txt',
+  'https://raw.githubusercontent.com/PolishFiltersTeam/KADhosts/master/KADhosts.txt',
   'https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Spam/hosts',
   'https://v.firebog.net/hosts/static/w3kbl.txt',
-  'https://www.dshield.org/feeds/suspiciousdomains_High.txt',
 
   // Advertising
-  'https://raw.githubusercontent.com/bigdargon/hostsVN/master/hosts',
+  'https://adaway.org/hosts.txt',
   'https://v.firebog.net/hosts/AdguardDNS.txt',
+  'https://v.firebog.net/hosts/Admiral.txt',
   'https://raw.githubusercontent.com/anudeepND/blacklist/master/adservers.txt',
+  'https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt',
   'https://v.firebog.net/hosts/Easylist.txt',
   'https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext',
   'https://raw.githubusercontent.com/FadeMind/hosts.extras/master/UncheckyAds/hosts',
-  'https://raw.githubusercontent.com/jdlingyu/ad-wars/master/hosts',
+  'https://raw.githubusercontent.com/bigdargon/hostsVN/master/hosts',
   // Malicious
   'https://raw.githubusercontent.com/DandelionSprout/adfilt/master/Alternate%20versions%20Anti-Malware%20List/AntiMalwareHosts.txt',
   'https://osint.digitalside.it/Threat-Intel/lists/latestdomains.txt',
   'https://s3.amazonaws.com/lists.disconnect.me/simple_malvertising.txt',
-  'https://mirror1.malwaredomains.com/files/justdomains',
-  'https://mirror.cedia.org.ec/malwaredomains/immortal_domains.txt',
-  'https://www.malwaredomainlist.com/hostslist/hosts.txt',
+  'https://v.firebog.net/hosts/Prigent-Crypto.txt',
   'https://bitbucket.org/ethanr/dns-blacklists/raw/8575c9f96e5b4a1308f2f12394abd86d0927a4a0/bad_lists/Mandiant_APT1_Report_Appendix_D.txt',
   'https://phishing.army/download/phishing_army_blocklist_extended.txt',
   'https://gitlab.com/quidsup/notrack-blocklists/raw/master/notrack-malware.txt',
@@ -40,18 +39,17 @@ const hosts = [
   // Tracking & Telemetry
   'https://v.firebog.net/hosts/Easyprivacy.txt',
   'https://v.firebog.net/hosts/Prigent-Ads.txt',
-  'https://gitlab.com/quidsup/notrack-blocklists/raw/master/notrack-blocklist.txt',
   'https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.2o7Net/hosts',
   'https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/hosts/spy.txt',
-  'https://www.github.developerdan.com/hosts/lists/ads-and-tracking-extended.txt',
   'https://hostfiles.frogeye.fr/firstparty-trackers-hosts.txt',
+  'https://raw.githubusercontent.com/Kees1958/W3C_annual_most_used_survey_blocklist/master/TOP_EU_US_Ads_Trackers_HOST',
 ];
 
-const removeDups = (names) => {
+const removeDuplicate = (names) => {
   const unique = {};
-  names.forEach((i) => {
-    if (!unique[i]) {
-      unique[i] = true;
+  names.forEach((index) => {
+    if (!unique[index]) {
+      unique[index] = true;
     }
   });
   return Object.keys(unique);
@@ -70,22 +68,24 @@ const parse = (data) => {
   return host.sort();
 };
 
-const parstHost = (host) => axios.get(host)
-  .then((response) => {
-    console.log('ok', host);
-    return parse(response.data);
-  });
-
 console.time('all');
 console.time('host');
-Promise.all(hosts.map(parstHost))
+Promise.all(
+  hosts.map((host) => axios.get(host)
+    .then((response) => {
+      console.log('ok', host);
+      return parse(response.data);
+    }, () => {
+      console.log('error', host);
+    })),
+)
   .then((listHost) => {
     console.timeEnd('host');
 
     console.time('parse');
     listHost.push(parse(readFileSync('./custom/dbl.txt', 'utf8')));
 
-    const myHost = removeDups(listHost.flat().sort());
+    const myHost = removeDuplicate(listHost.flat().sort());
     console.log(myHost.length);
 
     writeFileSync('./custom/host.txt', myHost.join('\n'));
